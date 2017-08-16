59d1bd54fd3a59b498fe40e46ede78656d4199be
aidatorajiro <kawarusosu@zoho.com>
2017-08-17 02:52:13 +0900

    update russell

--- russell.hs
+++ russell.hs
@@ -3,8 +3,11 @@ newtype Set x = Set (Set x -> Bool)
 unSet :: Set x -> (Set x -> Bool)
 unSet (Set x) = x
 
+isElement :: Set x -> Set x -> Bool
+isElement (Set f) = f
+
 russell :: Set x
 russell = Set (\(Set f) -> not $ f (Set f))
 
 main :: IO ()
-main = print $ unSet russell russell
+main = print $ isElement russell russell
