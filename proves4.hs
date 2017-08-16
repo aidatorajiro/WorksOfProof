3bd72749b6c83e969da60beb30abc5a99944d2c0
aidatorajiro <kawarusosu@zoho.com>
2017-08-17 02:34:06 +0900

    lint proves2 / add proves4

--- proves4.hs
+++ proves4.hs
@@ -1,20 +1,13 @@
 {-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ImpredicativeTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 
 -- [定義]
-data Not p
+-- Not
+newtype Not p = Not (forall q. p -> q)
 
 -- [公理]
 -- 排中律
-class Classic where
-    classic :: Either p (Not p)
-
--- pで矛盾が導かれるならばpでない、pでないならばpで矛盾が導かれる
-class Contradiction where
-    cton :: (forall q. p -> q) -> Not p
-    ntoc :: Not p -> (forall q. p -> q)
+classic :: Either p (n p)
+classic = undefined
 
 -- [証明]
 -- 三段論法
@@ -35,44 +28,50 @@ associativeLaw :: ((p -> q) -> r) -> (p -> (q -> r))
 associativeLaw h = const $ h . const
 
 -- P -> Pでない -> Q
-bomb :: Contradiction => p -> Not p -> q
-bomb p np = ntoc np p
+bomb :: p -> Not p -> q
+bomb p (Not pq) = pq p
 
 -- (Pかつ(Pでない))ならばQ
-bombAnd :: Contradiction => (p, Not p) -> q
+bombAnd :: (p, Not p) -> q
 bombAnd (p, np) = bomb p np
 
 -- ド・モルガンの法則
-deMorgan :: Contradiction => (Not p, Not q) -> Not (Either p q)
-deMorgan (np, nq) = cton (\pq ->
+deMorgan :: (Not p, Not q) -> Not (Either p q)
+deMorgan (np, nq) = Not (\pq ->
     case pq of
         Left  p -> bomb p np
         Right q -> bomb q nq )
 
 -- 二重否定法則
-pnnp :: Contradiction => p -> Not (Not p)
-pnnp p = cton $ bomb p
+pnnp :: p -> Not (Not p)
+pnnp p = Not $ bomb p
 
-nnpp :: (Contradiction, Classic) => Not (Not p) -> p
+nnpp :: Not (Not p) -> p
 nnpp nnp = case classic of
     Left  p  -> p
     Right np -> bomb np nnp
 
 -- 対偶
-contraposition1 :: Contradiction => (p -> q) -> (Not q -> Not p)
-contraposition1 h nq = cton (\p -> bomb (h p) nq)
+contraposition1 :: (p -> q) -> (Not q -> Not p)
+contraposition1 h nq = Not (\p -> bomb (h p) nq)
 
-contraposition2 :: (Contradiction, Classic) => (Not q -> Not p) -> (p -> q)
-contraposition2 h p = nnpp $ contraposition1 h (pnnp p)
+contraposition2 :: (Not q -> Not p) -> (p -> q)
+contraposition2 h p = nnpp $ (contraposition1 h) (pnnp p)
+
+--いろいろ
+implyToOr :: (p -> q) -> Either (n p) q
+implyToOr pq = case classic of
+    Left  r  -> Right $ pq r
+    Right nr -> Left nr
 
 -- パースの法則
-peirce_lemma1 :: (Contradiction, Classic) => ((p -> q) -> p) -> ((Not q -> Not p) -> p)
+peirce_lemma1 :: ((p -> q) -> p) -> ((Not q -> Not p) -> p)
 peirce_lemma1 x y = x (contraposition2 y)
 
-peirce_lemma2 :: (Contradiction, Classic) => (Not p -> p) -> p
+peirce_lemma2 :: (Not p -> p) -> p
 peirce_lemma2 x = either id x classic
 
-peirce :: (Contradiction, Classic) => ((p -> q) -> p) -> p
+peirce :: ((p -> q) -> p) -> p
 peirce h =
     peirce_lemma2
     (
