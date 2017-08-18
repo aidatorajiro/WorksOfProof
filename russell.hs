d1a7f8caf520b657a5ca57bcee2913c0cdcdf063
aidatorajiro <kawarusosu@zoho.com>
2017-08-18 23:02:28 +0900

    update russel

--- russell.hs
+++ russell.hs
@@ -1,13 +1,10 @@
 newtype Set x = Set (Set x -> Bool)
 
-unSet :: Set x -> (Set x -> Bool)
-unSet (Set x) = x
-
-isElement :: Set x -> Set x -> Bool
-isElement (Set f) = f
+elements :: Set x -> Set x -> Bool
+elements (Set f) = f
 
 russell :: Set x
 russell = Set (\(Set f) -> not $ f (Set f))
 
 main :: IO ()
-main = print $ isElement russell russell
+main = print $ russell `elements` russell
