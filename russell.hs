0be8ad5879fbb5e8e69efa7b72f8507ea9f0a02f
aidatorajiro <kawarusosu@zoho.com>
2017-08-19 16:04:18 +0900

    linted russel.hs

--- russell.hs
+++ russell.hs
@@ -4,7 +4,7 @@ elements :: Set x -> Either (Set x) x -> Bool
 elements (Set f) = f
 
 russell :: Set x
-russell = Set (\e -> either (\x -> not $ x `elements` (Left x)) (const False) e)
+russell = Set $ either (\x -> not $ x `elements` Left x) (const False)
 
 main :: IO ()
-main = print $ russell `elements` (Left russell)
+main = print $ russell `elements` Left russell
