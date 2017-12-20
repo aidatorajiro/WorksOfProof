e1482066b0c1d22828651d9d858d8aabff890c11
aidatorajiro <kawarusosu@zoho.com>
2017-12-20 12:54:29 +0900

    modified some files

--- proves4.hs
+++ proves4.hs
@@ -6,7 +6,7 @@ newtype Not p = Not (forall q. p -> q)
 
 -- [公理]
 -- 排中律
-classic :: Either p (n p)
+classic :: Either p (Not p)
 classic = undefined
 
 -- [証明]
@@ -37,51 +37,29 @@ bombAnd (p, np) = bomb p np
 
 -- ド・モルガンの法則
 deMorgan :: (Not p, Not q) -> Not (Either p q)
-deMorgan (np, nq) = Not (\pq ->
-    case pq of
-        Left  p -> bomb p np
-        Right q -> bomb q nq )
+deMorgan (np, nq) = Not $ either (`bomb` np) (`bomb` nq)
 
 -- 二重否定法則
 pnnp :: p -> Not (Not p)
 pnnp p = Not $ bomb p
 
 nnpp :: Not (Not p) -> p
-nnpp nnp = case classic of
-    Left  p  -> p
-    Right np -> bomb np nnp
+nnpp nnp = either id (`bomb` nnp) classic
 
 -- 対偶
 contraposition1 :: (p -> q) -> (Not q -> Not p)
 contraposition1 h nq = Not (\p -> bomb (h p) nq)
 
 contraposition2 :: (Not q -> Not p) -> (p -> q)
-contraposition2 h p = nnpp $ (contraposition1 h) (pnnp p)
+contraposition2 h p = nnpp $ contraposition1 h (pnnp p)
 
 --いろいろ
-implyToOr :: (p -> q) -> Either (n p) q
-implyToOr pq = case classic of
-    Left  r  -> Right $ pq r
-    Right nr -> Left nr
+implyToOr :: (p -> q) -> Either (Not p) q
+implyToOr pq = either (Right . pq) Left classic
 
 -- パースの法則
-peirce_lemma1 :: ((p -> q) -> p) -> ((Not q -> Not p) -> p)
-peirce_lemma1 x y = x (contraposition2 y)
-
-peirce_lemma2 :: (Not p -> p) -> p
-peirce_lemma2 x = either id x classic
-
 peirce :: ((p -> q) -> p) -> p
-peirce h =
-    peirce_lemma2
-    (
-        const
-        $
-        either
-        (h . const)
-        (peirce_lemma2 . associativeLaw (peirce_lemma1 h))
-        classic
-    )
+peirce h = either id (\(Not px) -> h px) classic
 
 -- なんか
 prop1 :: (forall p. ((p -> q) -> q)) -> ((p -> q) -> p) -> p
--- ski_03.hs
+++ ski_03.hs
@@ -1,7 +1,5 @@
 {-# LANGUAGE TypeOperators, MultiParamTypeClasses #-}
 
-import Unsafe.Coerce
-
 infixr 7 :->:
 infixl 7 #
 
@@ -13,26 +11,20 @@ class SKI where
   i :: a :->: a
   (#) :: (a :->: b) -> a -> b -- modus ponens
 
-newtype I a = I (I a :->: a)
-
-unI :: I a -> I a :->: a
-unI (I x) = x
-{-# NOINLINE unI #-}
-
 c :: SKI => (a :->: b :->: c) :->: b :->: a :->: c
 c = s#(b#b#s)#(k#k)
 
 b :: SKI => (b :->: c) :->: (a :->: b) :->: a :->: c
 b = s#(k#s)#k
 
-m :: SKI => I a :->: a
-m = (s unI)#i
+m :: SKI => (a :->: b) :->: b
+m = s#i#i
 
-l :: SKI => (a :->: b) :->: I a :->: b
+l :: SKI => (a :->: b) :->: (c :->: a) :->: b
 l = c#b#m
 
 y :: SKI => (a :->: a) :->: a
-y = s#l#(b#I#l)
+y = s#l#l
 
 --y :: SKI => (a :->: a) :->: a
 --y = s # (k # (s # i # i)) # (s # (s # (k # s) # k) # (k # (s # i # i)))
